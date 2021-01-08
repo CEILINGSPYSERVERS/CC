97019f392daa3062b93469015338e3c67ecb3ebc
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-08 01:31:57 -0600

    Update startup.lua

--- Turtle/startup.lua
+++ Turtle/startup.lua
@@ -1,6 +1,8 @@
 --wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/startup.lua startup.lua
 shell.execute("set","motd.enable","false")
 shell.execute("label","set","_Turtle")
+shell.execute("rm","position.lua")
+shell.execute("wget","https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua","position.lua")
 shell.execute("clear")
 print("Name: "..os.getComputerLabel())
 print("ID: "..os.getComputerID())
