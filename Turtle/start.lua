85d3a4bf0b5fc9df550bff2664b5fc6015456dc4
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-08 03:32:25 -0600

    Update start.lua

--- Turtle/start.lua
+++ Turtle/start.lua
@@ -9,7 +9,7 @@ shell.run("rm position.get.lua")
 
 --refreshes files
 shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua position.lua")
-shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.get.lua position.get.lua")
+shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/positionget.lua positionget.lua")
 
 --cd to home
 shell.run("cd ..")
@@ -21,5 +21,5 @@ shell.run("clear")
 print("Name: "..os.getComputerLabel())
 print("ID: "..os.getComputerID())
 print("Fuel: "..turtle.getFuelLevel())
---require("position.get.lua")
+require("ulib/positionget.lua")
 print("---------------------------------------")
