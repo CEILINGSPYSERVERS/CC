810107cfb927107d94d829387776155ff6b3a8c1
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-07 22:20:40 -0600

    Update startup.lua

--- GPS/startup.lua
+++ GPS/startup.lua
@@ -1,4 +1,4 @@
---https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/GPS/startup.lua
+--wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/GPS/startup.lua startup.lua
 shell.execute("set","motd.enable","false")
 shell.execute("clear")
 shell.execute("fg")
