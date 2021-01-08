c1a896c01faacd1c3fdf75dac50fcd7c156ec8de
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-08 00:13:05 -0600

    Update startup.lua

--- Turtle/startup.lua
+++ Turtle/startup.lua
@@ -1,4 +1,4 @@
---wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/GPS/startup.lua startup.lua
+--wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/startup.lua startup.lua
 shell.execute("set","motd.enable","false")
 shell.execute("label","set","_Turtle")
 shell.execute("clear")
