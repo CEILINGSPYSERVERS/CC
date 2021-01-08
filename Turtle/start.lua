8c2597511242805c96e4449c78b49dbd41df63b6
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-08 15:00:41 -0600

    Update start.lua

--- Turtle/start.lua
+++ Turtle/start.lua
@@ -6,17 +6,17 @@ shell.run("label set Turtle_"..os.getComputerID())
 --checks if files exist, removes them, and downloads them
 if fs.exists("ulib/position.lua")
     then fs.delete("ulib/position.lua")
-        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua ulib/position.lua")
+        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua position.lua")
     else
-        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua ulib/position.lua")
+        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua position.lua")
     end
 
 
 if fs.exists("ulib/positionget.lua")
     then fs.delete("ulib/positionget.lua")
-        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/positionget.lua ulib/positionget.lua")
+        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/positionget.lua positionget.lua")
     else
-        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/positionget.lua ulib/positionget.lua")
+        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/positionget.lua positionget.lua")
     end
 
 --cd to home
