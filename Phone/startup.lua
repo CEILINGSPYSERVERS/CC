47498108c5a820b0cce412c3103fd76ea840ff5e
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-08 02:13:29 -0600

    Update startup.lua

--- Phone/startup.lua
+++ Phone/startup.lua
@@ -1,7 +1,7 @@
 --wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Phone/startup.lua startup.lua
-shell.execute("set","motd.enable","false")
-shell.execute("label","set","CSP_Phone")
-shell.execute("clear")
+shell.run("set motd.enable false")
+shell.run("label set CSP_Phone")
+shell.run("clear")
 print("Name: "..os.getComputerLabel())
 print("ID: "..os.getComputerID())
 print("--------------------------")
