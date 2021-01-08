74fa771cf1ed819b51606b97be9798f7cb31809f
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-07 23:03:01 -0600

    Update startup.lua

--- GPS/startup.lua
+++ GPS/startup.lua
@@ -1,9 +1,9 @@
 --wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/GPS/startup.lua startup.lua
 shell.execute("set","motd.enable","false")
-shell.execute("label","set",""GPS @ X Y Z"")
+shell.execute("label","set","GPS")
 shell.execute("clear")
 print("Name: "..os.getComputerLabel())
 print("ID: "..os.getComputerID())
 print("--------------------------------------------------")
-shell.execute("fg")
+shell.execute("bg")
 shell.run("gps","host",X,Y,Z)
