2d4b4f195a7647a10dd755df2e93614407413d10
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-20 18:20:05 -0600

    Update start.lua

--- GPS/start.lua
+++ GPS/start.lua
@@ -4,9 +4,9 @@
 shell.run("label set GPS_"..os.getComputerID())
 
 --checks if files exist, removes them, and downloads them
-if not fs.exists("ulib/gpshost.txt")
+if not fs.exists("ulib/gpshost.lua")
     then
-        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/GPS/gpshost.lua gpshost.txt")
+        shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/GPS/gpshost.lua gpshost.lua")
     end
 
 --cd to home
