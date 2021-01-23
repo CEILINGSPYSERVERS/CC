f66d7696ef4a3f3dfe89c0babd41d729f70d5f8b
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-23 16:54:53 -0600

    Update facing.lua

--- Turtle/facing.lua
+++ Turtle/facing.lua
@@ -1,16 +1,21 @@
 --get and set initial position of turtle
 require("position")
 x1, z1 = x, z
+print(x, z)
+print(x1, z1)
 
 --move and get new position
 turtle.forward(1)
 sleep(1)
 require("position")
 x2, z2 = x, z
+print(x, z)
+print(x2, z2)
 
 --check the difference between the two positions
 x3 = x2 - x1
 z3 = z2 - z1
+print(x3, z3)
 
 --x face check
 if x3 ~= 0 then
@@ -33,5 +38,7 @@ end
 --reset position
 turtle.back(1)
 sleep(2)
+print(facing)
 
 require("position")
+print(x, z)
