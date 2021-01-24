81f0fddd9f8089926e8eaec85fcf01441b4f027e
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-24 00:17:20 -0600

    Update

--- Turtle/amine.lua
+++ Turtle/amine.lua
@@ -1,6 +1,6 @@
 require("facing")
-sleep(2)
 x2, y2, z2 = io.read("*n","*n","*n")
 mx = x2 - x
 my = y2 - y
 mz = z2 - z
+print(mx, my, mz)
--- Turtle/facing.lua
+++ Turtle/facing.lua
@@ -2,44 +2,35 @@
 require("position")
 position()
 x1, z1 = x, z
-print(x, z)
-print(x1, z1)
 
 --move and get new position
 turtle.forward(1)
 sleep(1)
 position()
 x2, z2 = x, z
-print(x, z)
-print(x2, z2)
 
 --check the difference between the two positions
 x3 = x2 - x1
 z3 = z2 - z1
-print(x3, z3)
 
 --x face check
 if x3 ~= 0 then
     if x3 == 1 then
-        facing = px
+        facing = "+x"
     else
-        facing = nx
+        facing = "-x"
     end
 end
 
 --y face check
 if z3 ~= 0 then
     if z3 == 1 then
-        facing = pz
+        facing = "+z"
     else
-        facing = nz
+        facing = "-z"
     end
 end
 
 --reset position
 turtle.back(1)
-sleep(2)
-print(facing)
-
 position()
-print(x, z)
