8e4a67e5221b9b355fbcefd4e2054f2e485ee45b
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-01-26 16:22:27 -0600

    Update

--- Turtle/start.lua
+++ Turtle/start.lua
@@ -6,38 +6,18 @@ shell.run("label set Turtle_"..os.getComputerID())
 --clears shell output
 shell.run("clear")
 
---checks if files exist, removes them, and downloads them and clears output
-if fs.exists("ulib/position.lua") then
-    fs.delete("ulib/position.lua")
-    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua position.lua")
+--gets ulibget file
+if fs.exists("ulib/ulibget.lua") then
+    fs.delete("ulib/ulibget.lua")
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/ulibget.lua ulibget.lua")
 else
-    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua position.lua")
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/ulibget.lua ulibget.lua")
 end
 shell.run("clear")
 
-if fs.exists("ulib/positionget.lua") then
-    fs.delete("ulib/positionget.lua")
-    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/positionget.lua positionget.lua")
-else
-    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/positionget.lua positionget.lua")
-end
-shell.run("clear")
-
-if fs.exists("ulib/amine.lua") then
-    fs.delete("ulib/amine.lua")
-    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/amine.lua amine.lua")
-else
-    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/amine.lua amine.lua")
-end
-shell.run("clear")
-
-if fs.exists("ulib/facing.lua") then
-    fs.delete("ulib/facing.lua")
-    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/facing.lua facing.lua")
-else
-    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/facing.lua facing.lua")
-end
-shell.run("clear")
+--runs new background shell to download files
+require("ulib/ulibget")
+shell.run("bg")
 
 --move current directory to home and clears output
 shell.run("cd ..")
---
+++ Turtle/ulibget.lua
@@ -0,0 +1,35 @@
+--checks if files exist, removes them, and downloads them and clears output
+if fs.exists("ulib/position.lua") then
+    fs.delete("ulib/position.lua")
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua position.lua")
+else
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/position.lua position.lua")
+end
+
+if fs.exists("ulib/positionget.lua") then
+    fs.delete("ulib/positionget.lua")
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/positionget.lua positionget.lua")
+else
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/positionget.lua positionget.lua")
+end
+
+if fs.exists("ulib/amine.lua") then
+    fs.delete("ulib/amine.lua")
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/amine.lua amine.lua")
+else
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/amine.lua amine.lua")
+end
+
+if fs.exists("ulib/facing.lua") then
+    fs.delete("ulib/facing.lua")
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/facing.lua facing.lua")
+else
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/facing.lua facing.lua")
+end
+
+if fs.exists("ulib/setface.lua") then
+    fs.delete("ulib/setface.lua")
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/setface.lua setface.lua")
+else
+    shell.run("wget https://raw.githubusercontent.com/CEILINGSPYSERVERS/CC/main/Turtle/setface.lua setface.lua")
+end
