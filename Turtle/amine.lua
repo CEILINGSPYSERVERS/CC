98a8fc856bcf4dbaf742ef6a0381abd76f84db10
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-05-27 23:46:29 -0500

    Update amine.lua

--- Turtle/amine.lua
+++ Turtle/amine.lua
@@ -19,6 +19,31 @@ mx = x2 - x
 my = y2 - y
 mz = z2 - z
 
+--checks fuel levels against requested area
+fLevel = turtle.getFuelLevel()
+a1 = mx * mz
+aT = a1 * my
+if fLevel < aT then
+    error("Not enough fuel")
+end
+
+--set turtle to known direction
+setface("+x")
+
+--does mining
+for 0,my,1 do
+    for,mz,1 do
+        for 0,mx,1 do
+            turtle.dig(left)
+        end
+        turtle.turnRight()
+        turtle.forward()
+        turtle.turnRight()
+    end
+    turtle.up()
+end
+
+--[[
 --calls setface function which turns turtle to correct direction
 if mx > my then
     if mz ~= 0 then
@@ -39,5 +64,4 @@ else
         end
     end
 end
-
---does mining
+]]
